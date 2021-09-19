b8e26402ddc36383b63996b926dad6885cbb645e
Lucas Bollella <lubolle@gmail.com>
2021-09-19 13:53:57 -0300

    Se agrega diagrama de tablas, archivo starUML, 2 transformaciones del pentaho dim_curso y fact_cursoEsperado

--- queries.sql
+++ queries.sql
@@ -1,23 +1,14 @@
-create database commodore64;
+CREATE TABLE `commodore64`.`dim_curso` (
+`id_curso` INT NOT NULL,
+`titulo` VARCHAR(255) NOT NULL,
+`codigo_curso` VARCHAR(255) NOT NULL);
 
-CREATE TABLE `commodore64`.`dim_docente` (
-  `id` INT NOT NULL,
-  `apellido` VARCHAR(255) NOT NULL,
-  `direccion` VARCHAR(255) NULL,
-  `ciudad` VARCHAR(255) NULL,
-  `estado` VARCHAR(255) NULL,
-  `fechacontratacion` VARCHAR(45) NULL,
-  `cantfamiliares` INT NULL,
-  `fechanacimiento` VARCHAR(45) NULL,
-  `cantidadhijos` INT NULL,
-  `comision` DECIMAL NULL,
-  `comisionrate` FLOAT NULL,
-  PRIMARY KEY (`id`));
-
-CREATE TABLE `commodore64`.`dim_salas` (
-  `id_sala` INT NOT NULL,
-  `nro_sala` VARCHAR(45) NULL,
-  `capacidad` INT NULL,
-  `ubicacion` VARCHAR(255) NULL,
-  PRIMARY KEY (`nro_sala`));
-
+CREATE TABLE `commodore64`.`fact_cursoEsperado` (
+  `id_cursoesperado` INT UNIQUE,
+  `id_curso` VARCHAR(255) NOT NULL,
+  `capacidad_maxima` INT NOT NULL,
+  `id_temporada` VARCHAR(255) NOT NULL,
+ PRIMARY KEY (`id_cursoesperado`),
+ FOREIGN KEY(`id_curso`) REFERENCE dim_curso (id_curso)
+ );
+  
