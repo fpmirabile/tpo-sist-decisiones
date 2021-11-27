147a79fb9a2ef3f578b655e2e8c5eccb3b6f445a
Ivo <istrficek@uade.edu.ar>
2021-11-27 16:27:11 -0300

    Cambio en las transformaciones para poder correrlas repetidamente y elimine un par de errores de consistencia

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 create database if not exists commodore64;
 
 CREATE TABLE `commodore64`.`dim_docente` (
-  `id_docente` INT NOT NULL,
+  `id_docente` INT NOT NULL UNIQUE,
   `apellido` VARCHAR(255) NOT NULL,
   `direccion` VARCHAR(255) NULL,
   `ciudad` VARCHAR(255) NULL,
@@ -15,7 +15,7 @@ CREATE TABLE `commodore64`.`dim_docente` (
   PRIMARY KEY (`id_docente`));
 
 CREATE TABLE `commodore64`.`dim_salas` (
-  `id_sala` INT NOT NULL,
+  `id_sala` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nro_sala` VARCHAR(45) NULL,
   `capacidad` INT NULL,
   `ubicacion` VARCHAR(255) NULL,
@@ -23,35 +23,23 @@ CREATE TABLE `commodore64`.`dim_salas` (
   PRIMARY KEY (`id_sala`));
 
 
-CREATE TABLE `commodore64`.dim_curso (
-`id_curso` INT NOT NULL,
+CREATE TABLE `commodore64`.`dim_curso` (
+`id_curso` INT NOT NULL UNIQUE AUTO_INCREMENT,
 `titulo` VARCHAR(255) NOT NULL,
 `codigo_curso` VARCHAR(255) NOT NULL,
 `temporada` VARCHAR(255) NOT NULL,
 PRIMARY KEY (`id_curso`));
 
- 
- /*CREATE TABLE commodore64.dim_modalidad (
-	id_modalidad INT auto_increment NOT NULL,
-	descripcion varchar(100) NOT NULL,
-	dias varchar(100) NOT NULL,
-	CONSTRAINT dim_modalidad_PK PRIMARY KEY (id_modalidad)
-)
-
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;*/
-
-CREATE TABLE commodore64.dim_tiempo (
-	id_tiempo INT auto_increment NOT NULL,
-	ano INT NOT NULL,
-	temporada VARCHAR(50) NOT NULL,
+CREATE TABLE `commodore64`.`dim_tiempo` (
+	`id_tiempo` INT NOT NULL UNIQUE AUTO_INCREMENT,
+	`ano` INT NOT NULL,
+	`temporada` VARCHAR(50) NOT NULL,
 	CONSTRAINT dim_tiempo_PK PRIMARY KEY (id_tiempo)
 );
 
 
-CREATE TABLE `commodore64`.fact_curso_esperado (
-  `id_curso_esperado` INT UNIQUE,
+CREATE TABLE `commodore64`.`fact_curso_esperado` (
+  `id_curso_esperado` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `id_curso` INT NOT NULL,
   `capacidad_maxima` INT NOT NULL,
   `id_tiempo` INT NOT NULL,
@@ -61,16 +49,16 @@ CREATE TABLE `commodore64`.fact_curso_esperado (
  );
  
  
-CREATE TABLE commodore64.fact_curso_real (
-	id_curso_real INT auto_increment,
-	id_curso INT NOT NULL,
-	id_docente INT NOT NULL,
-	id_tiempo INT NOT NULL,
-	id_sala INT NOT NULL,
-	modalidad varchar(100) NOT NULL,
-	dias varchar(100) NOT NULL,
-	inscriptos INT NOT NULL,
-  duracion varchar(100) NOT NULL,
+CREATE TABLE `commodore64`.`fact_curso_real` (
+	`id_curso_real` INT NOT NULL UNIQUE AUTO_INCREMENT,
+	`id_curso` INT NOT NULL,
+	`id_docente` INT NOT NULL,
+	`id_tiempo` INT NOT NULL,
+	`id_sala` INT NOT NULL,
+	`modalidad` varchar(100) NOT NULL,
+	`dias` varchar(100) NOT NULL,
+	`inscriptos` INT NOT NULL,
+	`duracion` varchar(100) NOT NULL,
 	CONSTRAINT fact_cursoReal_PK PRIMARY KEY (id_curso_real),
 	CONSTRAINT fact_cursoReal_FK FOREIGN KEY (id_curso) REFERENCES commodore64.dim_curso(id_curso),
 	CONSTRAINT fact_cursoReal_FK_1 FOREIGN KEY (id_docente) REFERENCES commodore64.dim_docente(id_docente),
